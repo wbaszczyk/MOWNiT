1ee953fa2fffd2f3d2f79f65773adce0add5db87
gitprofit <liszcz.michal@gmail.com>
2013-05-08 20:48:47 +0200

    RequestQueue -> lista

--- src/RequestQueue.java
+++ src/RequestQueue.java
@@ -1,23 +1,32 @@
 import java.util.*;
 
 public class RequestQueue {
-
-	// prosty wrapper na wbudowana kolejke
-	// + synchronizowane wstawianie
-
-	Queue<Request> pending;
+	
+	List<Request> pending;
 
 	public RequestQueue() {
 
-		pending = new ArrayDeque<>();
+		pending = new ArrayList<>();
 	}
 
-	public synchronized void push(Request request) {
+	public synchronized void pushBack(Request request) {
 		pending.add(request);
 	}
 
-	public synchronized Request pop() {
-		return pending.remove();
+	public synchronized Request popFront() {
+		return pending.remove(0);
+	}
+	
+	public Request get(int pos) {
+		return pending.get(pos);
+	}
+	
+	public synchronized void remove(int pos) {
+		pending.remove(pos);
+	}
+	
+	public synchronized void insert(Request request, int pos) {
+		pending.add(pos,  request);
 	}
 
 	public synchronized boolean isEmpty() {
