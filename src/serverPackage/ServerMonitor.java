eafb03c938ec8e2a41d8febf228536898e1d10d2
gitprofit <liszcz.michal@gmail.com>
2013-05-18 14:03:56 +0200

    ServerMonitor utworzony

---
+++ src/serverPackage/ServerMonitor.java
@@ -0,0 +1,15 @@
+package serverPackage;
+
+public class ServerMonitor implements Runnable {
+	
+	private FileSystem system;
+	
+	public ServerMonitor(FileSystem system)
+	{
+		//
+	}
+
+	@Override
+	public void run() {
+	}
+}
